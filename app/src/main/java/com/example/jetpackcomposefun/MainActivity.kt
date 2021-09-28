83492dc3064c45fa58efbc3b122347e20bffe8b7
artur <uniumuniu@gmail.com>
2021-09-28 10:30:42 +0200

    list with scrolling and simple animations

--- app/src/main/java/com/example/jetpackcomposefun/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposefun/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.jetpackcomposefun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,7 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Greeting("Android")
+                MyScreenContent()
             }
         }
     }
@@ -37,11 +39,58 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface {
+fun MyScreenContent(names: List<String> = List(1000) { "Android $it" }) {
+    var counterState by remember {
+        mutableStateOf(0)
+    }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NamesList(names = names, modifier = Modifier.weight(1f))
+        Counter(
+            count = counterState,
+        ) { count -> counterState = count }
+
+        if (counterState > 5) {
+            Text(text = "I love to count!")
+        }
+    }
+}
+
+@Composable
+fun NamesList(names: List<String>, modifier: Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) {
+            GreetingItem(name = it)
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun Counter(count: Int, modifier: Modifier = Modifier, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }, modifier = modifier) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Composable
+fun GreetingItem(name: String) {
+    var isSelected by remember {
+        mutableStateOf(false)
+    }
+
+    val targetColor by animateColorAsState(
+        targetValue = if (isSelected) MaterialTheme.colors.primary else Color.Transparent,
+        animationSpec = tween(durationMillis = 4000)
+    )
+
+    Surface(color = targetColor) {
         Text(
             text = "Hello $name!",
-            modifier = Modifier.padding(16.5.dp)
+            modifier = Modifier
+                .clickable { isSelected = !isSelected }
+                .padding(16.5.dp)
+                .fillMaxWidth()
         )
     }
 }
@@ -50,6 +99,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MyApp {
-        Greeting("Android")
+        MyScreenContent()
     }
 }
